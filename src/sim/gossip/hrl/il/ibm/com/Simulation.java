7bee8e7482f7bb39f65e0373f310c7d9e3e4bc5e
Artem Barger <BARTEM@il.ibm.com>
2017-05-24 12:02:16 +0300

    Small improvements (#1)
    * Small improvements
    
    Signed-off-by: Artem Barger <bartem@il.ibm.com>
    
    * Move random outside while loop
    
    * Remove unrelated comment

--- src/sim/gossip/hrl/il/ibm/com/Simulation.java
+++ src/sim/gossip/hrl/il/ibm/com/Simulation.java
@@ -1,110 +1,97 @@
 package sim.gossip.hrl.il.ibm.com;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Simulation {
 
-	private int h;
-	private int k;
-	private int n;
-	private int lastRound;
-	private boolean[] peers;
-	private Set<Integer> newInfections = new HashSet<Integer>();
-	private LinkedList<Integer> infectedPeers = new LinkedList<Integer>();
-	private int infectedCount;
+    private int h;
+    private int k;
+    private int n;
+    private int lastRound;
+    private boolean[] peers;
+    private Set<Integer> newInfections = new HashSet<Integer>();
+    private LinkedList<Integer> infectedPeers = new LinkedList<Integer>();
+    private int infectedCount;
 
-	public Simulation(int h, int k, int n) {
-		if (k > n) {
-			throw new RuntimeException("k(" + k + ") must be less than n(" + n + ")");
-		}
-		this.h = h;
-		this.k = k;
-		this.n = n;
-		this.peers = new boolean[n];
-	}
+    public Simulation(int h, int k, int n) {
+        if (k > n) {
+            throw new RuntimeException("k(" + k + ") must be less than n(" + n + ")");
+        }
+        this.h = h;
+        this.k = k;
+        this.n = n;
+        this.peers = new boolean[n];
+    }
 
-	public void run() {
-		lastRound = (int) (n * Math.log(n));
-		// Some peer is infected at first
-		infectedPeers.add((int) (Math.random() * n));
-		for (int i = 0; i < lastRound && infectedCount < n; i++) {
-			round(i);
-		}
-		System.out.println("Infected:" + infectedCount + " out of " + n);
-	}
+    public void run() {
+        lastRound = (int) (n * Math.log(n));
+        // Some peer is infected at first
+        infectedPeers.add((int) (Math.random() * n));
+        for (int i = 0; i < lastRound && infectedCount < n; i++) {
+            round(i);
+        }
+        System.out.println("Infected:" + infectedCount + " out of " + n);
+    }
 
-	private void round(int r) {
-		System.out.println("Running simulation round " + r + " out of " + lastRound);
-		if (r <= h) {
-			forwardingPhase();
-			return;
-		}
-		pullPhase();
-	} // round
+    private void round(int r) {
+        System.out.println("Running simulation round " + r + " out of " + lastRound);
+        if (r <= h) {
+            forwardingPhase();
+            return;
+        }
+        pullPhase();
+    } // round
 
-	private void forwardingPhase() {
-		while (!infectedPeers.isEmpty()) {
-			int p = infectedPeers.removeFirst();
-			// Peer has already been infected
-			if (peers[p]) {
-				continue;
-			}
-			peers[p] = true;
-			infectedCount++;
-			for (int q : selectRandomPeers(k, n)) {
-				// If current peer has not been infected yet
-				newInfections.add(q);
-			}
-		} // while
+    private void forwardingPhase() {
+        while (!infectedPeers.isEmpty()) {
+            int p = infectedPeers.removeFirst();
+            // Peer has already been infected
+            if (peers[p]) {
+                continue;
+            }
+            peers[p] = true;
+            infectedCount++;
 
-		// Add all new infections to the infection queue
-		newInfections.stream().forEach(p -> {
-			infectedPeers.add(p);
-		});
-		newInfections.clear();
-	} // forwardingPhase
+            newInfections.addAll(selectRandomPeers(k, n));
+        } // while
 
-	private void pullPhase() {
-		for (int p = 0; p < peers.length; p++) {
-			// Already infected, nothing to do
-			if (peers[p]) {
-				continue;
-			}
-			// Else select some peers to pull the message from
-			boolean pullSucceeded = Arrays.stream(selectRandomPeers(k, n)).anyMatch(q -> peers[q]);
-			if (pullSucceeded) {
-				peers[p] = true;
-				infectedCount++;
-			}
-		}
-	} // pullPhase
+        // Add all new infections to the infection queue
+        infectedPeers.addAll(newInfections);
+        newInfections.clear();
+    } // forwardingPhase
 
-	private static int[] selectRandomPeers(int k, int n) {
-		int[] peers = new int[k];
-		// N >> k so it's ok not to check corner cases
-		for (int i = 0; i < k; i++) {
-			while (true) {
-				boolean exists = false;
-				int p = (int) (Math.random() * n);
-				for (int j = 0; j < i; j++) {
-					if (peers[j] == p) {
-						exists = true;
-						break;
-					} // if
-				} // for j < i
-				if (!exists) {
-					peers[i] = p;
-					break;
-				}
-			} // while
-		} // for i < n
-		return peers;
-	}
+    private void pullPhase() {
+        for (int p = 0; p < peers.length; p++) {
+            // Already infected, nothing to do
+            if (peers[p]) {
+                continue;
+            }
+            // Else select some peers to pull the message from
+            boolean pullSucceeded = selectRandomPeers(k, n).stream().anyMatch(q -> peers[q]);
+            if (pullSucceeded) {
+                peers[p] = true;
+                infectedCount++;
+            }
+        }
+    } // pullPhase
 
-	public static void main(String[] args) {
-		new Simulation(70, 6, 1000).run();
-	}
+    private List<Integer> selectRandomPeers(int k, int n) {
+        List<Integer> peers = new ArrayList<>();
+        // N >> k so it's ok not to check corner cases
+        Random rnd = new Random(System.currentTimeMillis());
+        while (peers.size() < k) {
+            int p = rnd.nextInt(n);
+            if (peers.contains(p)) {
+                continue;
+            }
+            peers.add(p);
+        } 
+        return peers;
+    }
+
+    public static void main(String[] args) {
+        new Simulation(70, 6, 1000).run();
+    }
 }
